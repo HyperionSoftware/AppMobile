3cb9e5c8c5c1df799991e6d970ffb5b6c8288242
JoanBonell <joanbonellruiz@gmail.com>
2023-02-19 13:40:45 +0100

    AboutPage done with a button in MainActivity

--- app/src/main/java/udl/cat/hyperion/appmobils/thegame/MainActivity.java
+++ app/src/main/java/udl/cat/hyperion/appmobils/thegame/MainActivity.java
@@ -2,13 +2,32 @@ package udl.cat.hyperion.appmobils.thegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button aboutPageButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aboutPageButton = (Button) findViewById(R.id.button);
+        aboutPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityAboutPage();
+            }
+        });
+    }
+
+    public void openActivityAboutPage(){
+        Intent intent = new Intent(this, AboutPage.class);
+        startActivity(intent);
     }
 }
