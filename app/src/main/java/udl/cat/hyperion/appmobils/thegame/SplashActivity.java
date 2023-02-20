9899f653b80d225a5c88dd9721c64a5b20bb1205
JoanBonell <joanbonellruiz@gmail.com>
2023-02-20 01:10:16 +0100

    SplashScreen solved.

---
+++ app/src/main/java/udl/cat/hyperion/appmobils/thegame/SplashActivity.java
@@ -0,0 +1,14 @@
+package udl.cat.hyperion.appmobils.thegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+    }
+}
